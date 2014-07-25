805420c9cb0c9077b9e07d73f02e10c095fef1ed
Edouard Oyallon <edouard.oyallon@ens.fr>
2014-07-25 11:29:15 +0200

    added test function

---
+++ lsd_1.6/Matlab/test_lsd.m
@@ -0,0 +1,14 @@
+X=double(imread('input.png'));
+X=floor(256*X/max(max(max(X))));% IL FAUT QUE L'IMAGE SOIT ENTRE 0 ET 255(pas 0 et 1)
+% IL FAUT QUE L'IMAGE SOIT EN NIVEAU DE GRIS
+% Vérifier que des inputs un peu aléatoires ne font pas planter le soft(en
+% entrée, une image en couleur double(ie NxMx3) fait planter le soft)
+output_lsd=lsd(X);
+figure,
+hold on
+imagesc(X);
+colormap gray;
+for i=1:size(output_lsd,1)
+    plot([output_lsd(i,1),output_lsd(i,3)],[output_lsd(i,2),output_lsd(i,4)]);
+end
+hold off
