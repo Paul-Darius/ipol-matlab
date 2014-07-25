4587cb8146a789e515fee4d1f810afbd45293f53
Paul-Darius <paul-darius.sarmadi@telecom-sudparis.eu>
2014-07-25 11:51:48 +0200

    Update libmexipol.h

--- mex-ipol/libmexipol.h
+++ mex-ipol/libmexipol.h
@@ -7,7 +7,7 @@ char* get_string(const mxArray* prhsi);
 void send_string(char* string, const mxArray* plhsi);
 
 /* Structure functions */
-void get_structure(const mxArray* prhsi, char* tableau); // if data is type double we have to convert string to double
+void get_structure(const mxArray* prhsi, char* array); // if data is type double we have to convert string to double
 void send_double_array(double* array, const mxArray* plhsi); // send a double array to an element from the structure
 void send_matrix(double* array, int num_col, int num_row, const mxArray* plhsi);
 void send_string_array(char* array, const mxArray* plhsi);
