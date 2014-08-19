1a156978ac0f9e0b2eee86901ba318d9e19dc125
Paul-Darius <paul-darius.sarmadi@telecom-sudparis.eu>
2014-08-19 14:59:49 +0200

    lib.c

--- mex-ipol/libmexipol.c
+++ mex-ipol/libmexipol.c
@@ -2,10 +2,11 @@
 //  libmexipol.c
 //  
 //
-//  Created by Paul-Darius Sarmadi on 19/08/2014.
+//  Created by Paul-Darius Sarmadi.
 //
 //
-typedef char* char16_t;
+
+#include "libmexipol.h"
 #include "mex.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,3 +24,80 @@ void numberio(int nlhs, int i, int nrhs, int j)
         mexErrMsgTxt("The number of input arguments is not correct");
     }
 }
+
+void type(char *type, const mxArray *prhs[], int i)
+{
+    if (strcmp (type,"double")==0)
+    {
+        if (!mxIsDouble(prhs[i]))
+        {
+            char* numero[200];
+            sprintf(numero, "%d", i);
+            char* errmsg[200];
+            strcpy (errmsg, "The type of the data number ");
+            strcat (errmsg, numero);
+            strcat (errmsg, " must be double.");
+            mexErrMsgTxt(errmsg);
+
+        }
+    }
+    else if (strcmp (type,"string")==0)
+    {
+        if (!mxIsClass(prhs[i],"mxCHAR_CLASS"))
+        {
+            char* numero[200];
+            sprintf(numero, "%d", i);
+            char* errmsg[200];
+            strcpy (errmsg, "The type of the data number ");
+            strcat (errmsg, numero);
+            strcat (errmsg, " must be string.");
+            mexErrMsgTxt(errmsg);
+
+        }
+    }
+    else
+    {
+        mexErrMsgTxt("Ask the programmer of this soft to clear up this issue");
+    }
+}
+
+int isCorrectString( char* requiredString, mxArray *prhsi)
+{
+    char* buf;
+    size_t buflen;
+    int status;
+    buflen = mxGetN(prhsi)*sizeof(char)+1;
+    buf = (char*)malloc(buflen);
+    status = mxGetString(prhsi, buf, (mwSize)buflen);
+    if (strcmp(buf,requiredString)!=0)
+    {
+        return 0;
+    }
+    else return 1;
+}
+
+
+/* String functions */
+
+
+int get_string_length(mxArray* prhsi)
+{
+    return mxGetN(prhsi);
+}
+
+char* get_string(mxArray* prhsi)
+{
+    char* buf;
+    size_t buflen;
+    int status;
+    buflen = mxGetN(prhsi)*sizeof(char)+1;
+    buf = (char*)malloc(buflen);
+    status = mxGetString(prhsi, buf, (mwSize)buflen);
+    return buf;
+}
+
+void set_string(char* message, mxArray* prhsi)
+{
+    prhsi=mxCreateString(message);
+}
+/* */
