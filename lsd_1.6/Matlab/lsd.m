7a419aef437c794e4ba6acc2e48cfd7bb46f396f
root <paul-darius.sarmadi@telecom-sudparis.eu>
2014-09-19 22:02:47 +0200

    Update lsd.m

--- lsd_1.6/Matlab/lsd.m
+++ lsd_1.6/Matlab/lsd.m
@@ -2,19 +2,25 @@ function out=lsd(image,option)
 % LSD  return an array containing datas concerning line segments detected
 % in an image
 %   out = LSD(A) returns line segments belonging to A
-% A must be a double matrix
+%
+% INPUT :
+% =======
+%
+% A must be a double matrix.
+% To load it from an image on your computer -which has to be a .pgm file- use the following command :
+% A=double(imread('adress/of/file.pgm'))
 %
 % OPTIONS :
 % =========
 %
-% Even if default options are optimized, you can change them.
+% Even if default options are optimized, you can change them as you wish.
 % Just use a structure containing them or the ones you need.
 % For example, if you want a scale equals to one and a sigma_coef equals to 0.7, type the following command :
 %   option.scale=1;
 %   option.sigma_coef=0.8;
 %   out = LSD(A,option)
 %
-% You can name your structure as you wish. However its components have to respect some rules :
+% You can name your structure as you want. However its components have to respect some rules :
 %
 % SCALE : 
 %   name as a component of the optional structure : scale
@@ -23,6 +29,7 @@ function out=lsd(image,option)
 %   minimum value : 0.0
 %   maximum value : -
 %   Scale image by Gaussian filter before processing.
+%
 % SIGMA COEFFICIENT : 
 %   name as a component of the optional structure : sigma_coef
 %   type : double	
@@ -30,6 +37,7 @@ function out=lsd(image,option)
 %   minimum value : 0.0
 %   maximum value : -
 %   Sigma for Gaussian filter is computed as sigma_coef/scale.
+%
 % QUANTIZATION : 
 %   name as a component of the optional structure : quant
 %   type : double	
@@ -37,6 +45,7 @@ function out=lsd(image,option)
 %   minimum value : 0.0
 %   maximum value : -
 %   Bound to quantization error on the gradient norm.
+%
 % ANGLE TOLERANCE : 
 %   name as a component of the optional structure : ang_th
 %   type : double	
@@ -44,6 +53,7 @@ function out=lsd(image,option)
 %   minimum value : 0.0
 %   maximum value : 180.0
 %   Gradient angle tolerance in degrees.
+%
 % DETECTION THRESHOLD : 
 %   name as a component of the optional structure : log_eps
 %   type : double	
@@ -51,6 +61,7 @@ function out=lsd(image,option)
 %   minimum value : -
 %   maximum value : -
 %   Detection threshold, -log10(max. NFA)
+%
 % DENSITY : 
 %   name as a component of the optional structure : density_th
 %   type : double	
@@ -58,6 +69,7 @@ function out=lsd(image,option)
 %   minimum value : 0.0
 %   maximum value : 1.0
 %   Minimal density of region points in a rectangle to be accepted.
+%
 % NUMBER OF BINS : 
 %   name as a component of the optional structure : n_bins
 %   type : integer
@@ -75,7 +87,7 @@ function out=lsd(image,option)
 %   The second column contains y1
 %   The third column contains x2
 %   The fourth column contains y2
-%   The three last columns contain other informations available on the following adress :
+%   The three last columns contain other informations available at the following adress :
 %   http://www.ipol.im/pub/art/2012/gjmr-lsd/
     if (~exist('option', 'var'))
         out=MEX_lsd_cmd(image);
